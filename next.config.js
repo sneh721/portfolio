a87421483b1055838cabcb30eca0fb05037fafa3
Sneh Gupta <snehgupta721@gmail.com>
2024-05-19 16:30:25 +0530

    Path

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production'
  * https://stackoverflow.com/questions/60452054/nextjs-deploy-to-a-specific-url-path
  */
 function getBasePath() {
-  var basePath = undefined
+  var basePath = '/portfolio.\n'
 
   if (isProd && process.env.BASE_PATH) {
     if (process.env.BASE_PATH.startsWith("/")) {
