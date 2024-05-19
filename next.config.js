fcc0e1ddb3ac5233b4ba8e32ddddea08bc70595d
Sneh Gupta <snehgupta721@gmail.com>
2024-05-19 16:41:37 +0530

    Pathing

--- next.config.js
+++ next.config.js
@@ -6,7 +6,18 @@ const isProd = process.env.NODE_ENV === 'production'
  * https://stackoverflow.com/questions/60452054/nextjs-deploy-to-a-specific-url-path
  */
 function getBasePath() {
-  return '/portfolio.\n'
+  function getBasePath() {
+    var basePath = undefined
+
+    if (isProd && process.env.BASE_PATH) {
+      if (process.env.BASE_PATH.startsWith("/")) {
+        basePath = process.env.BASE_PATH;
+      } else {
+        basePath = "/" + process.env.BASE_PATH;
+      }
+    }
+    return basePath
+  }
 }
 
 const basePath = getBasePath()
