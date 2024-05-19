ce4237d0adf3647382e7bfa68ff85547454a714f
Sneh Gupta <snehgupta721@gmail.com>
2024-05-19 16:47:03 +0530

    ahhhh

--- components/Footer.js
+++ components/Footer.js
@@ -30,8 +30,6 @@ export const Contact = ({ title, description, buttons }) => {
 	);
 }
 
-import Link from 'next/link';
-
 export const Footer = () => {
 	return (
 		<footer className="bg-secondary text-center py-2 px-5">
